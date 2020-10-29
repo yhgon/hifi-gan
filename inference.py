ba215c57ef7ddff57db97790feb50e026d2a3fbd
Hyungon_Ryu <yhgon@users.noreply.github.com>
2020-10-29 22:59:51 +0900

    Update inference.py

--- inference.py
+++ inference.py
@@ -58,24 +58,20 @@ def inference(a):
     generator.remove_weight_norm()
     toc_ready = time.time() 
 
-
-
-
     with torch.no_grad():
         ## warm up 
         print("warm up..." , end='')
         for i, filname in enumerate(filelist):
-            if i >=2 : 
-                return
-            wav, sr = load_wav(os.path.join(a.input_wavs_dir, filname))
-            wav = wav / MAX_WAV_VALUE
-            wav = torch.FloatTensor(wav).to(device)
+            if i ==0 : 
+                wav, sr = load_wav(os.path.join(a.input_wavs_dir, filname))
+                wav = wav / MAX_WAV_VALUE
+                wav = torch.FloatTensor(wav).to(device)
 
-            x = get_mel(wav.unsqueeze(0))
-            y_g_hat = generator(x)
-            audio = y_g_hat.squeeze()
-            audio = audio * MAX_WAV_VALUE
-            toc_gen = time.time()          
+                x = get_mel(wav.unsqueeze(0))
+                y_g_hat = generator(x)
+                audio = y_g_hat.squeeze()
+                audio = audio * MAX_WAV_VALUE
+                toc_gen = time.time()          
         print("done")
         for i, filname in enumerate(filelist):
             tic_loadwav = time.time()
